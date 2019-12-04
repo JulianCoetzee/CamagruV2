2577e923e20323f35689be8a33fd7138d315495d
Julian COETZEE <jcoetzee@c5r2s4.wethinkcode.co.za>
2019-12-04 15:07:17 +0200

    img saves working

---
+++ webphp/save.php
@@ -0,0 +1,67 @@
+<?php
+
+// if (isset($_POST['canvasData']))
+if (isset($_POST['img']))
+{
+
+  // Get the data
+  $username = $_SESSION['username'];
+  // $imageData = $_POST['canvasData'];
+  $imageData = $_POST['img'];
+
+  $savename = $username.date_default_timezone_set('Africa/Johannesburg');
+  if (!$savename || !$imageData)
+  {
+    echo "<script>alert('Save error!')</script>";
+		echo "<script>window.open('../webpages/camlive.php?error=sendingfailed','_self')</script>";
+		exit();
+  }
+ 
+  // Remove the headers (data:,) part.
+  $imageData = str_replace(' ','+',$imageData);
+  $filtersave=substr($imageData, strpos($imageData, ",")+1);
+ 
+  // Need to decode before saving since the data received is already base64 encoded
+  $unencodedsave=base64_decode($filtersave);
+ 
+  $savefile = fopen($savename.'.png', 'wb');
+  // $savefile = fopen('/goinfre/jcoetzee/Desktop/PHP_live/apache2/htdocs/CamagruTakeTwo/new3.png', 'w');
+  fwrite($savefile, $unencodedsave);
+  fclose($savefile);
+
+  if (!$savefile)
+  {
+    echo "<script>alert('Save error!')</script>";
+		echo "<script>window.open('../webpages/camlive.php?error=savingfailed','_self')</script>";
+		exit();
+  }
+  else
+  {
+    require 'database2.php';
+
+    $stmt = $conn->prepare("INSERT INTO `feed`(`image_id`, `img`, `username`, `upload_date`, `likes`) VALUES(:imgid, :img, :user, :savedate, :likes)");
+		$stmt->bindParam(':imgid', $savename);
+    $stmt->bindParam(':img', $savefile);
+    $stmt->bindParam(':user', $username);
+    $stmt->bindParam(':savedate', date_default_timezone_set('Africa/Johannesburg'));
+    $stmt->bindParam(':likes', 0);
+		if (!$stmt->execute()) 
+		{
+			echo "<script>alert('SQL ERROR: 1')</script>";
+			echo "<script>window.open('../webpages/camlive.php?error=sqlerror','_self')</script>";
+			exit();
+    }
+    else
+    {
+      echo "<script>window.open('../webpages/camlive.php?save=success','_self')</script>";
+      exit();
+    }
+  }
+}
+else
+{
+  echo "<script>window.open('../webpages/camlive.php','_self')</script>";
+  exit();
+}
+
+?>
