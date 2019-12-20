25ec5951126cd6878f35106087ebdb99fd027a80
Julian COETZEE <jcoetzee@c5r2s4.wethinkcode.co.za>
2019-12-20 13:54:13 +0200

    minor changes. will complete soon

--- scripts/feedscripts.js
+++ scripts/feedscripts.js
@@ -1,13 +1,18 @@
-function commdisplay(img_id) {
-    if (window.XMLHttpRequest) {
+function commdisplay(img_id) 
+{
+    if (window.XMLHttpRequest) 
+    {
         // IE7+, Firefox, Chrome, Opera, Safari compatibility
         xmlhttp = new XMLHttpRequest();
-    } else {
+    } 
+    else 
+    {
         // IE6, IE5 compatability
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200)
+        {
             document.getElementById(`comments_section-${img_id}`).innerHTML = this.responseText;
         }
     };
@@ -15,27 +20,39 @@ function commdisplay(img_id) {
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("details=" + img_id);
 }​
-function comm_img(img_id) {
-    if (window.XMLHttpRequest) {
+
+function comm_img(img_id) 
+{
+    if (window.XMLHttpRequest) 
+    {
         xmlhttp = new XMLHttpRequest();
-    } else {
+    } 
+    else 
+    {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText === "False") {
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            if (this.responseText === "False")
+            {
                 //false
                 document.getElementById("notification").value = "<div style='background: rgba(255,0,0, 0.7); border-radius:5px;'>Could not post comment!</div>"
                 setTimeout(() => {
                     document.getElementById("notification").innerHTML = "";
                 }, 3000);
-            } else if (this.responseText === "False1") {
+            }
+            else if (this.responseText === "False1")
+            {
                 //false
                 document.getElementById("notification").innerHTML = "<div style='background: rgba(255,0,0, 0.7); border-radius:5px;'>Please type in a valid comment less than 200 characters.</div>"
                 setTimeout(() => {
                     document.getElementById("notification").innerHTML = "";
                 }, 3000);
-            } else {
+            }
+            else
+            {
                 //true
                 document.getElementById(`comment_box-${img_id}`).value = "";
                 document.getElementById("notification").innerHTML = "<div style='background: rgba(0, 151, 19, 0.7); border-radius:5px;'>Comment Posted!</div>"
@@ -50,21 +67,29 @@ function comm_img(img_id) {
     let comm = document.getElementById(`comment_box-${img_id}`).value;
     xmlhttp.send("comment=submit&comment_box=" + comm + "&id=" + img_id);
 }​
-function like_img(img_id) {
-    if (window.XMLHttpRequest) {
+function like_img(img_id)
+{
+    if (window.XMLHttpRequest)
+    {
         xmlhttp = new XMLHttpRequest();
-    } else {
+    }
+    else
+    {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText === "False") {
+        if (this.readyState == 4 && this.status == 200)
+        {
+            if (this.responseText === "False")
+            {
                 //false
                 document.getElementById("notification").innerHTML = "<div style='background: rgba(255,0,0, 0.7); border-radius:5px;'>Action denied!</div>"
                 setTimeout(() => {
                     document.getElementById("notification").innerHTML = "";
                 }, 3000);
-            } else {
+            }
+            else
+            {
                 document.getElementById(`like_section-${img_id}`).innerHTML = this.responseText;
             }
         }
